88ce94fb4b9d6041f8e838c21777978cfe3578a4
juan narvaez <jdnarvaez@udenar.edu.co>
2023-10-11 03:24:24 -0500

    app clima

--- src/tools/ajax.js
+++ src/tools/ajax.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import "@madzadev/audio-player/dist/index.css";
 
 export const ajax = async (options) =>
   await axios.request(options).then((response) => response.data);
